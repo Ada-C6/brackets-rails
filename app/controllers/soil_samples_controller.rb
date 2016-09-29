e30b051bb447829a0a98d9c5a95daaa5e984bbd8
Kari Bancroft <kariabancroft@gmail.com>
2016-09-29 13:59:08 -0700

    Add new form and save data to the database

--- app/controllers/soil_samples_controller.rb
+++ app/controllers/soil_samples_controller.rb
@@ -11,5 +11,15 @@ class SoilSamplesController < ApplicationController
   end
 
   def create
+    @sample = SoilSample.new
+    @sample.location = params[:location]
+    @sample.scientist = params[:scientist]
+    if @sample.save
+      # SAVED SUCCESSFULLY
+      redirect_to soil_samples_path
+    else
+      # DID NOT SAVE
+      render :new
+    end
   end
 end
